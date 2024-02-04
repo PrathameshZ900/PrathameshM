4187abea2e2f2e9d54f1fd4be0a2434adf09b2b4
prathamesh magar <prathameshma1@gmail.com>
2024-02-05 00:41:46 +0530

    ok

--- script.js
+++ script.js
@@ -38,25 +38,24 @@ var swiper = new Swiper('.swiper-container', {
       clickable: true,
   },
 });
-let currentIndex = 0;
-    const container = document.querySelector('.about-containers');
-    const totalProjects = document.querySelectorAll('.details-container').length;
 
-    function showProject(index) {
-        const itemWidth = document.querySelector('.details-container').offsetWidth;
-        container.style.transform = `translateX(-${index * itemWidth}px)`;
-    }
+function sendMessage() {
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let subject = document.getElementById("subject").value;
+    let message = document.getElementById("message").value;
+    
+    
+    let body = "Name :" + name + "</br> E-mail :" + email + "</br> Subject :" + subject + "</br> Message :"+ message
 
-    function nextProject() {
-        currentIndex = (currentIndex + 1) % totalProjects;
-        showProject(currentIndex);
-    }
-
-    function prevProject() {
-        currentIndex = (currentIndex - 1 + totalProjects) % totalProjects;
-        showProject(currentIndex);
-    }
-
-    // Add event listeners for next and previous buttons
-    document.addEventListener('DOMContentLoaded', showProject(currentIndex));
-    document.querySelector('.arrow').addEventListener('click', nextProject);
+    Email.send({
+        SecureToken :"ce895eed-26fd-4fd3-a7b3-20036bf7197f",
+        Password : "482AE971BE4C9D61FEC11A563AA97A75326F",
+        To : 'prathameshma1@gmail.com',
+        From : "prathameshma1@gmail.com",
+        Subject : "Message From Contact From",
+        Body : body
+    }).then(
+      message => alert(message)
+    );
+  }
