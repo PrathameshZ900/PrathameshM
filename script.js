c87e1892199a0a5f0e48fb25002bf6c9e5caedde
prathamesh magar <prathameshma1@gmail.com>
2024-02-27 17:30:58 +0530

    okbrother

--- script.js
+++ script.js
@@ -5,17 +5,20 @@ function toggleMenu() {
     icon.classList.toggle("open");
   }
 
-// 
-function sendMessage() {
-    let name = document.getElementById("name").value;
-    let email = document.getElementById("email").value;
-    let subject = document.getElementById("subject").value;
-    let message = document.getElementById("message").value;
-    
-    
-    let body = "Name :" + name + "</br> E-mail :" + email + "</br> Subject :" + subject + "</br> Message :"+ message
 
-    Email.send({
+function sendMessage() {
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let phone = document.getElementById("subject").value; // Changed ID to match HTML
+  let message = document.getElementById("message").value;
+
+  // Properly formatted HTML for the body of the email
+  let body = "<p><strong>Name:</strong> " + name + "</p>" +
+             "<p><strong>Email:</strong> " + email + "</p>" +
+             "<p><strong>Phone:</strong> " + phone + "</p>" +
+             "<p><strong>Message:</strong> " + message + "</p>";
+
+  Email.send({
         SecureToken :"ce895eed-26fd-4fd3-a7b3-20036bf7197f",
         Password : "482AE971BE4C9D61FEC11A563AA97A75326F",
         To : 'prathameshma1@gmail.com',
